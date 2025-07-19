6b62b262a0e10b6b01c1b473dfd13c85d6366a5a
Nesh011001 <nehemiahkiprimo@gmail.com>
2025-07-19 11:09:39 +0300

    bb

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,14 +6,14 @@ def list_books(request):
     list_books = [f"{book.title} by {book.author}" for book in books]
     return render(request, "relationship_app/list_books.html", {"books": books})
 
-from django.shortcuts import render
+
 from django.views.generic import DetailView                                
 from relationship_app.models import Library  
 
 class LibraryBooksView(DetailView):
     model = Library
     context_object_name = 'library'
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
 
     def get_queryset(self):
         library_name = self.request.GET.get('library_name', 'Default Library')

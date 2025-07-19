9439ec21254c09a433ab4b2335748e2e50a581ff
Nesh011001 <nehemiahkiprimo@gmail.com>
2025-07-19 11:16:35 +0300

    ff

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from relationship_app.models import Book
 from relationship_app.models import Library
+from django.views.generic import DetailView 
 
 def list_books(request):
     books = Book.objects.all()
@@ -8,7 +9,7 @@ def list_books(request):
     return render(request, "relationship_app/list_books.html", {"books": books})
 
 
-from django.views.generic import DetailView 
+
 from .models import Library, Book                               
   
 class LibraryBooksView(DetailView):

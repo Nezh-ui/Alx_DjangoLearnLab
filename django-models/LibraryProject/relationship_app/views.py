f4209eaa3941cf901fb15ccece8909656e6c7968
Nesh011001 <nehemiahkiprimo@gmail.com>
2025-07-19 10:46:47 +0300

    r

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,7 +7,7 @@ from relationship_app.models import Book, Library
 def list_books(request):
     library_name = request.GET.get('library_name', 'Default Library')
     books = Book.objects.filter(library__name=library_name)
-    return render(request, 'list_books.html', {"books": books})
+    return render(request, 'relationship_app/list_books.html', {"books": books})
 
 from query_samples import List_all_books_in_library
 from django.views.generic import ListView

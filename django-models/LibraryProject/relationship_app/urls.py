aca0c0b40d71e10b48222e778ec9fe7cae302ff7
Nesh011001 <nehemiahkiprimo@gmail.com>
2025-07-20 09:25:07 +0300

    done

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('library/<str:library_name>/books/', LibraryDetailView.as_view(), name='library_books'),
     path('login/', LoginView.as_view(template_name= 'relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name= 'relationship_app/logout.html'), name='logout'),
-    path('register/', RegisterView.as_view(), name='register'),
+    path('register/', RegisterView.as_view(template_name='relationship_app/register.html'), name='register'),
 ]
 # This code defines the URL patterns for the relationship app in a Django project.
 # It includes a path for listing all books and a detail view for books in a specific library

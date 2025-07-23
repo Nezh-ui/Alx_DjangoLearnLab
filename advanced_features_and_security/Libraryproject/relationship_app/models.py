6931117d1f882b2fd684d60d1d4e410c85d40583
Nesh011001 <nehemiahkiprimo@gmail.com>
2025-07-23 10:21:41 +0300

    2121

---
+++ advanced_features_and_security/Libraryproject/relationship_app/models.py
@@ -0,0 +1,73 @@
+from django.conf import settings
+from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
+from django.contrib.auth.decorators import user_passes_test
+
+from typing import Any
+from django.contrib.auth.models import UserManager      
+
+class CustomUserManager(UserManager):
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username, email, password, **extra_fields)
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    
+    objects = CustomUserManager()
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
+    
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    role = models.CharField(max_length=100, choices=[('Admin', 'Admin'), ('Librarian', 'Librarian'), ('Member', 'Member')])
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+
+

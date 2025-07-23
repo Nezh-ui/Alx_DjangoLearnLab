88d83eb73d5198e2d669554f5e4a6cb173b8dea4
Nesh011001 <nehemiahkiprimo@gmail.com>
2025-07-23 10:33:39 +0300

    dzea

--- advanced_features_and_security/Libraryproject/bookshelf/admin.py
+++ advanced_features_and_security/Libraryproject/bookshelf/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
-from .models import Book
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(Book)
+# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = (
+        *UserAdmin.fieldsets,
+        (None, {'fields': ('date_of_birth', 'profile_picture')})
 
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')
-    list_filter = ('author', 'publication_year')
-    search_fields = ('title', 'author')
+    )
+
+    add_fieldsets = (
+        *UserAdmin.add_fieldsets,
+        (None, {'fields': ('date_of_birth', 'profile_picture')}),
+    )
+    
+
+admin.site.register(CustomUser, CustomUserAdmin)
+from django.contrib.auth.decorators import user_passes_test

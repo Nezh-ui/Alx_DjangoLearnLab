2b001eca848c405124b29f58af7c7da592aff0fc
Nesh011001 <nehemiahkiprimo@gmail.com>
2025-07-24 10:28:39 +0300

    hvvv

--- advanced_features_and_security/Libraryproject/bookshelf/models.py
+++ advanced_features_and_security/Libraryproject/bookshelf/models.py
@@ -19,7 +19,7 @@ class CustomUserManager(UserManager):
 
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
     
     objects = CustomUserManager()
 

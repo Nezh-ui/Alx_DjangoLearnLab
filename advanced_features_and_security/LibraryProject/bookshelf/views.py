3b44e173514f93dbbdbdb99ff34410de1abb20b5
Nesh011001 <nehemiahkiprimo@gmail.com>
2025-07-27 18:44:19 +0300

    udsnv

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import permission_required
 from .models import Book
 from .models import UserProfile
+from .forms import ExampleForm
 
 # Create your views here.
 UserProfile.objects.filter(user__username='desired_username')
@@ -35,5 +36,15 @@ def delete_book(request, book_id):
     return render(request, 'bookshelf/delete_book.html', {'book': book})
 
 
+def example_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # Process the form data here (e.g., save to the database)
+            return redirect('success_page')  # Change this to your actual success page
+    else:
+        form = ExampleForm()
+    
+    return render(request, 'form_template.html', {'form': form})
 
 

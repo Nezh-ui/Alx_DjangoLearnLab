a4e557ecae1187125b1f63be7d089a346b957f0f
Nesh011001 <nehemiahkiprimo@gmail.com>
2025-08-01 10:15:44 +0300

    Rename BookListView to Book and update corresponding URL reference

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from api.views import BookListView
+from .views import BookList
 
 urlpatterns = [
-    path('books/', BookListView.as_view(), name='book-list'),
+    path('books/', BookList.as_view(), name='book-list'),
 ]
 # This file is used to define the API URLs for the application.
-# It includes the URL for the BookListView which lists all books.
+# It includes the URL for the BookList which lists all books.

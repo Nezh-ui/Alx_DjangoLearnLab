162b6898ff198b950f4534f609dc973d480b8cbd
Nesh011001 <nehemiahkiprimo@gmail.com>
2025-08-08 09:13:28 +0300

    Add manage.py for Django project initialization and management

---
+++ advanced-api-project/advanced_api_project/api/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Author(models.Model): # Model for Author
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model): # Model for Book
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE) # ForeignKey to Author
+    publication_year = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.title
